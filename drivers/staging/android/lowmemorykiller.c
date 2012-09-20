96391fe3248c5271f55b8540a84d8e0300c5d423
Liam Mark <lmark@codeaurora.org>
2012-09-20 14:42:28 -0700

    PARTIAL PICK: android/lowmemorykiller: Check all tasks for death pending
    The lowmemorykiller uses the TIF_MEMDIE flag to help ensure it doesn't
    kill another task until the memory from the previously killed task has
    been returned to the system.
    
    However the lowmemorykiller does not currently look at tasks who do not
    have a tasks->mm, but just because a process doesn't have a tasks->mm
    does not mean that the task's memory has been fully returned to the
    system yet.
    
    In order to prevent the lowmemorykiller from unnecessarily killing
    multiple applications in a row the lowmemorykiller has been changed to
    ensure that previous killed tasks are no longer in the process list
    before attempting to kill another task.
    
    Change-Id: I7d8a8fd39ca5625e6448ed2efebfb621f6e93845
    Signed-off-by: Liam Mark <lmark@codeaurora.org>
    [vinmenon@codeaurora.org: Use task_lmk_waiting instead of TIF_MEMDIE
    as it is no more set for tasks killed my LMK]
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -425,6 +425,20 @@ static void mark_lmk_victim(struct task_struct *tsk)
 	}
 }
 
+static int test_task_lmk_waiting(struct task_struct *p)
+{
+	struct task_struct *t = p;
+ 	do {
+		task_lock(t);
+		if (task_lmk_waiting(t)) {
+			task_unlock(t);
+			return 1;
+		}
+		task_unlock(t);
+	} while_each_thread(p, t);
+ 	return 0;
+}
+
 static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 {
 	struct task_struct *tsk;
