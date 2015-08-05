4a647b857a9a9eabdecfb5cdee892e256764e715
Uladzislau Rezki <urezki@gmail.com>
2015-08-05 16:20:50 +0200

    sched: check pinned tasks before balance
    The problem is there are pinned tasks in the system
    which can not be migrated on another CPUs while performing
    load balancing. For example: 'kworker', 'ksoftirqd' and
    user space applications.
    
    In order to not disturb idle CPUs in that cases we check
    if CPU can be balanced before making actual decision
    to kick idle CPU and do nohz balance.
    
    This is done for power saving.
    
    Signed-off-by: Uladzislau Rezki <urezki@gmail.com>
    
    [ab123321]
    Update from Sony 51.1.A.3.159 kernel copyleft release.
    
    Signed-off-by: Artem Labazov <123321artyom@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5108,6 +5108,9 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 	if (!se) {
 		add_nr_running(rq, 1);
+
+		if (unlikely(p->nr_cpus_allowed == 1))
+			rq->nr_pinned_tasks++;
 	}
 
 #ifdef CONFIG_SMP
@@ -5218,6 +5221,9 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 	if (!se) {
 		sub_nr_running(rq, 1);
+
+		if (unlikely(p->nr_cpus_allowed == 1))
+			rq->nr_pinned_tasks--;
 	}
 
 #ifdef CONFIG_SMP
@@ -10722,10 +10728,26 @@ static inline bool nohz_kick_needed(struct rq *rq, bool only_update)
 	if (time_before(now, nohz.next_balance))
 		return false;
 
-	/*
-	 * If energy aware is enabled, do idle load balance if runqueue has
-	 * at least 2 tasks and cpu is overutilized
-	 */
+	if (unlikely(rq->nr_pinned_tasks > 0)) {
+		int delta = rq->nr_running - rq->nr_pinned_tasks;
+
+		/*
+		 * Check if it is possible to "unload" this CPU in case
+		 * of having pinned/affine tasks. Do not disturb idle
+		 * core if one of the below condition is true:
+		 *
+		 * - there is one pinned task and it is not "current"
+		 * - all tasks are pinned to this CPU
+		 */
+		if (delta < 2)
+			if (current->nr_cpus_allowed > 1 || !delta)
+				return false;
+	}
+
+        /*
+         * If energy aware is enabled, do idle load balance if runqueue has
+         * at least 2 tasks and cpu is overutilized
+         */
 	if (rq->nr_running >= 2 &&
 	    (!energy_aware() || cpu_overutilized(cpu)))
 		return true;
