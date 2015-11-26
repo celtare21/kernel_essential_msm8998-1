2a31326fca15f2e95ba1ee806275aeecf2733005
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2015-11-25 18:56:00 -0800

    rcu: Allow expedited grace periods to be disabled at init
    Expedited grace periods can speed up boot, but are undesirable in
    aggressive real-time systems.  This commit therefore introduces a
    kernel parameter rcupdate.rcu_normal_after_boot that disables
    expedited grace periods just before init is spawned.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/rcu/update.c
+++ kernel/rcu/update.c
@@ -63,6 +63,9 @@
 module_param(rcu_expedited, int, 0);
 module_param(rcu_normal, int, 0);
 
+static int rcu_normal_after_boot;
+module_param(rcu_normal_after_boot, int, 0);
+
 #ifdef CONFIG_DEBUG_LOCK_ALLOC
 /**
  * rcu_read_lock_sched_held() - might we be in RCU-sched read-side critical section?
@@ -180,6 +183,8 @@ EXPORT_SYMBOL_GPL(rcu_unexpedite_gp);
 void rcu_end_inkernel_boot(void)
 {
 	rcu_unexpedite_gp();
+	if (rcu_normal_after_boot)
+		WRITE_ONCE(rcu_normal, 1);
 }
 
 #ifdef CONFIG_PREEMPT_RCU
