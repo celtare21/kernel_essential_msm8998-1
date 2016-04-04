c6ffa716ff9a7ffd604f7aeaf85f285e8bc11576
Rabin Vincent <rabinv@axis.com>
2016-04-04 15:42:02 +0200

    sched/debug: Don't dump sched debug info in SysRq-W
    sysrq_sched_debug_show() can dump a lot of information.  Don't print out
    all that if we're just trying to get a list of blocked tasks (SysRq-W).
    The information is still accessible with SysRq-T.
    
    Signed-off-by: Rabin Vincent <rabinv@axis.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1459777322-30902-1-git-send-email-rabin.vincent@axis.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5249,7 +5249,8 @@ void show_state_filter(unsigned long state_filter)
 	}
 
 #ifdef CONFIG_SYSRQ_SCHED_DEBUG
-	sysrq_sched_debug_show();
+	if (!state_filter)
+		sysrq_sched_debug_show();
 #endif
 	rcu_read_unlock();
 	/*
