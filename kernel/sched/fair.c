93848d254c2b7e710760a2e99b611009dc4414bc
Srikar Dronamraju <srikar@linux.vnet.ibm.com>
2016-04-06 18:47:40 +0530

    sched/fair: Fix asym packing to select correct CPU
    When asymmetric packing is set in the sched_domain and target CPU is
    busy, update_sd_pick_busiest() may not select the busiest runqueue.
    When target CPU is busy, find_busiest_group() will ignore checks for
    asym packing and may continue to load balance using the currently
    selected not-the-busiest runqueue as source runqueue.
    Selecting the busiest runqueue as source when the target CPU is busy,
    should result in achieving much better load balance.
    
    Also when target CPU is not busy and asymmetric packing is set in sd,
    select higher CPU as source CPU for load balancing.
    
    While doing this change, move the check to see if target CPU is busy
    into check_asym_packing().
    
    The extent of performance benefit from this change decreases with the
    increasing load. However there is benefit in undercommit as well as
    overcommit conditions.
    
    1. Record per second ebizzy (32 threads) on a 64 CPU power 7 box. (5 iterations)
    4.6.0-rc2
    	Testcase:         Min         Max         Avg      StdDev
    	  ebizzy:  5223767.00 10368236.00  7946971.00  1753094.76
    
    4.6.0-rc2+asym-changes
    	Testcase:         Min         Max         Avg      StdDev     %Change
    	  ebizzy:  8617191.00 13872356.00 11383980.00  1783400.89     +24.78%
    
    2. Record per second ebizzy (64 threads) on a 64 CPU power 7 box. (5 iterations)
    4.6.0-rc2
    	Testcase:         Min         Max         Avg      StdDev
    	  ebizzy:  6497666.00 18399783.00 10818093.20  4051452.08
    
    4.6.0-rc2+asym-changes
    	Testcase:         Min         Max         Avg      StdDev     %Change
    	  ebizzy:  7567365.00 19456937.00 11674063.60  4295407.48      +4.40%
    
    3. Record per second ebizzy (128 threads) on a 64 CPU power 7 box. (5 iterations)
    4.6.0-rc2
    	Testcase:         Min         Max         Avg      StdDev
    	  ebizzy: 37073983.00 40341911.00 38776241.80  1259766.82
    
    4.6.0-rc2+asym-changes
    	Testcase:         Min         Max         Avg      StdDev     %Change
    	  ebizzy: 38030399.00 41333378.00 39827404.40  1255001.86      +2.54%
    
    Signed-off-by: Srikar Dronamraju <srikar@linux.vnet.ibm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Gautham R Shenoy <ego@linux.vnet.ibm.com>
    Cc: Michael Neuling <mikey@neuling.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vaidyanathan Srinivasan <svaidy@linux.vnet.ibm.com>
    Link: http://lkml.kernel.org/r/1459948660-16073-1-git-send-email-srikar@linux.vnet.ibm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8951,6 +8951,9 @@ asym_packing:
 	if (!(env->sd->flags & SD_ASYM_PACKING))
 		return true;
 
+	/* No ASYM_PACKING if target cpu is already busy */
+	if (env->idle == CPU_NOT_IDLE)
+		return true;
 	/*
 	 * ASYM_PACKING needs to move all the work to the lowest
 	 * numbered CPUs in the group, therefore mark all groups
@@ -8960,7 +8963,8 @@ asym_packing:
 		if (!sds->busiest)
 			return true;
 
-		if (group_first_cpu(sds->busiest) > group_first_cpu(sg))
+		/* Prefer to move from highest possible cpu's work */
+		if (group_first_cpu(sds->busiest) < group_first_cpu(sg))
 			return true;
 	}
 
@@ -9127,6 +9131,9 @@ static int check_asym_packing(struct lb_env *env, struct sd_lb_stats *sds)
 	int busiest_cpu;
 
 	if (!(env->sd->flags & SD_ASYM_PACKING))
+		return 0;
+
+	if (env->idle == CPU_NOT_IDLE)
 		return 0;
 
 	if (!sds->busiest)
@@ -9386,8 +9393,7 @@ static struct sched_group *find_busiest_group(struct lb_env *env)
 	busiest = &sds.busiest_stat;
 
 	/* ASYM feature bypasses nice load balance check */
-	if ((env->idle == CPU_IDLE || env->idle == CPU_NEWLY_IDLE) &&
-	    check_asym_packing(env, &sds))
+	if (check_asym_packing(env, &sds))
 		return sds.busiest;
 
 	/* There is no busy sibling group to pull tasks from */
