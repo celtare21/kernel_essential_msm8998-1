a24fb84f9bae2212caf2839ca4665238b00200f4
Peter Zijlstra <peterz@infradead.org>
2016-10-03 16:20:59 +0200

    sched/core: Add missing update_rq_clock() in post_init_entity_util_avg()
    Address this rq-clock update bug:
    
      WARNING: CPU: 0 PID: 0 at ../kernel/sched/sched.h:797 post_init_entity_util_avg()
      rq->clock_update_flags < RQCF_ACT_SKIP
    
      Call Trace:
        __warn()
        post_init_entity_util_avg()
        wake_up_new_task()
        _do_fork()
        kernel_thread()
        rest_init()
        start_kernel()
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10819,6 +10819,7 @@ void online_fair_sched_group(struct task_group *tg)
 		se = tg->se[i];
 
 		raw_spin_lock_irq(&rq->lock);
+                update_rq_clock(rq);
 		post_init_entity_util_avg(se);
 		sync_throttle(tg, i);
 		raw_spin_unlock_irq(&rq->lock);
