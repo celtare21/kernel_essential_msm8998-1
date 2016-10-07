e38ea84085bfa876349ac1267e05e5faa916833c
Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
2016-10-07 16:59:03 -0700

    mm, oom: enforce exit_oom_victim on current task
    There are no users of exit_oom_victim on !current task anymore so enforce
    the API to always work on the current.
    
    Link: http://lkml.kernel.org/r/1472119394-11342-8-git-send-email-mhocko@kernel.org
    Signed-off-by: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Signed-off-by: Michal Hocko <mhocko@suse.com>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Vladimir Davydov <vdavydov@parallels.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- include/linux/oom.h
+++ include/linux/oom.h
@@ -74,7 +74,7 @@ extern int oom_kills_count(void);
 extern void note_oom_kill(void);
 extern bool out_of_memory(struct oom_control *oc);
 
-extern void exit_oom_victim(struct task_struct *tsk);
+extern void exit_oom_victim(void);
 
 extern int register_oom_notifier(struct notifier_block *nb);
 extern int unregister_oom_notifier(struct notifier_block *nb);
