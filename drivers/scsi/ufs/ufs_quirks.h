eca0e5e7e0a406ba27a387e04199dd91e66a5b20
Subhash Jadavani <subhashj@codeaurora.org>
2016-12-15 17:09:35 -0800

    scsi: ufs: read device descriptor once
    We are reading the device descriptor once for applying the device
    quirks and then device descriptor isn't saved anywhere for future
    use. Now that we also need the bDeviceSubClass parameter (of device
    descriptor), this change refactors the code to read the device
    descriptor once and save the imprortant parameters for later use
    by driver.
    
    Change-Id: Icb5d0380a9dcac0e7bd042d9be3abd8bcf6ead48
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/scsi/ufs/ufs_quirks.h
+++ drivers/scsi/ufs/ufs_quirks.h
@@ -35,32 +35,23 @@
 #define UFS_MODEL_SAMSUNG_MAX_QDEPTH	16
 
 /**
- * ufs_card_info - ufs device details
- * @wmanufacturerid: card details
- * @model: card model
- */
-struct ufs_card_info {
-	u16 wmanufacturerid;
-	char *model;
-};
-
-/**
  * ufs_card_fix - ufs device quirk info
  * @card: ufs card details
  * @quirk: device quirk
  */
 struct ufs_card_fix {
-	struct ufs_card_info card;
+	u16 w_manufacturer_id;
+	char *model;
 	unsigned int quirk;
 };
 
-#define END_FIX { { 0 } , 0 }
+#define END_FIX { 0 }
 
 /* add specific device quirk */
 #define UFS_FIX(_vendor, _model, _quirk) \
 		{						  \
-				.card.wmanufacturerid = (_vendor),\
-				.card.model = (_model),		  \
+				.w_manufacturer_id = (_vendor),\
+				.model = (_model),		  \
 				.quirk = (_quirk),		  \
 		}
 
