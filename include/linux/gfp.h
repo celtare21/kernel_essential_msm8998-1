3c780f1e194ab7b66bcac35212e7d24fc87f8aab
Lucas Stach <l.stach@pengutronix.de>
2017-02-24 14:58:37 -0800

    mm: alloc_contig_range: allow to specify GFP mask
    Currently alloc_contig_range assumes that the compaction should be done
    with the default GFP_KERNEL flags.  This is probably right for all
    current uses of this interface, but may change as CMA is used in more
    use-cases (including being the default DMA memory allocator on some
    platforms).
    
    Change the function prototype, to allow for passing through the GFP mask
    set by upper layers.
    
    Also respect global restrictions by applying memalloc_noio_flags to the
    passed in flags.
    
    Link: http://lkml.kernel.org/r/20170127172328.18574-1-l.stach@pengutronix.de
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: Radim Krcmar <rkrcmar@redhat.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Chris Zankel <chris@zankel.net>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Alexander Graf <agraf@suse.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- include/linux/gfp.h
+++ include/linux/gfp.h
@@ -550,7 +550,7 @@ static inline bool pm_suspended_storage(void)
 
 /* The below functions must be run on a range from a single zone. */
 extern int alloc_contig_range(unsigned long start, unsigned long end,
-			      unsigned migratetype);
+			      unsigned migratetype, gfp_t gfp_mask);
 extern void free_contig_range(unsigned long pfn, unsigned nr_pages);
 
 /* CMA stuff */
