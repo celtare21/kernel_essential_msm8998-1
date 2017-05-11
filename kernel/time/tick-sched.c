9ff21c7687f6115728729ca322d116ae18e10f45
Frederic Weisbecker <fweisbec@gmail.com>
2017-05-11 16:36:19 +0200

    nohz: Add hrtimer sanity check
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -815,8 +815,13 @@ static ktime_t tick_nohz_stop_sched_tick(struct tick_sched *ts,
 	tick.tv64 = expires;
 
 	/* Skip reprogram of event if its not changed */
-	if (ts->tick_stopped && (expires == dev->next_event.tv64))
-		goto out;
+	if (ts->tick_stopped) {
+		if (hrtimer_active(&ts->sched_timer))
+			WARN_ON_ONCE(hrtimer_get_expires(&ts->sched_timer) < dev->next_event);
+
+		if (expires == dev->next_event)
+			goto out;
+	}
 
 	/*
 	 * nohz_stop_sched_tick can be called several times before
