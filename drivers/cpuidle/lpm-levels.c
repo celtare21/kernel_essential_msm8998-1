82593323201a852421e98605448df8a390adbd7c
Lina Iyer <ilina@codeaurora.org>
2017-07-17 11:50:25 -0600

    drivers: cpuidle: lpm-levels: Fix KW issues with idle state idx < 0
    Idle state calculcations will need to return the state chosen as an
    integer. The state chosen is used as a index into the array and as such
    cannot be negative value. Do not return negative errors from the
    calculations. By default, the state returned wil be zero.
    
    Change-Id: Idb18e933f385cf868fe99fa6a2783f6b8e84c196
    Signed-off-by: Lina Iyer <ilina@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -692,7 +692,7 @@ static void update_history(struct cpuidle_device *dev, int idx);
 static int cpu_power_select(struct cpuidle_device *dev,
 		struct lpm_cpu *cpu)
 {
-	int best_level = -1;
+	int best_level = 0;
 	uint32_t latency_us = pm_qos_request_for_cpu(PM_QOS_CPU_DMA_LATENCY,
 							dev->cpu);
 	s64 sleep_us = ktime_to_us(tick_nohz_get_sleep_length());
@@ -706,8 +706,6 @@ static int cpu_power_select(struct cpuidle_device *dev,
 	uint32_t *min_residency = get_per_cpu_min_residency(dev->cpu);
 	uint32_t *max_residency = get_per_cpu_max_residency(dev->cpu);
 
-	if (!cpu)
-		return -EINVAL;
 
 	idx_restrict = cpu->nlevels + 1;
 
@@ -1527,17 +1525,11 @@ static int lpm_cpuidle_select(struct cpuidle_driver *drv,
 		struct cpuidle_device *dev)
 {
 	struct lpm_cluster *cluster = per_cpu(cpu_cluster, dev->cpu);
-	int idx;
 
 	if (!cluster)
 		return 0;
 
-	idx = cpu_power_select(dev, cluster->cpu);
-
-	if (idx < 0)
-		return -EPERM;
-
-	return idx;
+	return cpu_power_select(dev, cluster->cpu);
 }
 
 static void update_history(struct cpuidle_device *dev, int idx)
@@ -1581,9 +1573,6 @@ static int lpm_cpuidle_enter(struct cpuidle_device *dev,
 	const struct cpumask *cpumask = get_cpu_mask(dev->cpu);
 	int64_t start_time = ktime_to_ns(ktime_get()), end_time;
 	struct power_params *pwr_params;
-
-	if (idx < 0)
-		return -EINVAL;
 
 	pwr_params = &cluster->cpu->levels[idx].pwr;
 
