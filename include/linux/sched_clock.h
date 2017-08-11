b12465281cbc14f2989687c754c47c40c1b83e4f
Stephen Boyd <sboyd@codeaurora.org>
2017-08-11 14:23:19 -0700

    timers, sched/clock: Hook into s2idle freeze path
    The sched_clock() suspend/resume callbacks are called from a
    syscore_ops structure that's registered after the timekeeping
    syscore_ops. With the suspend-to-idle method of suspend we will
    freeze the tick and timekeeping subsystem, but we don't suspend
    the sched clock here, because syscore ops are not called in
    suspend to idle. Hook this up directly in the timekeeping freeze
    path when we go idle on the last CPU. This way, the sched clock
    doesn't jump across a suspend/resume cycle.
    
    Change-Id: I2ea488dce7186d6210d3af34b0e1a67c9f1cdf78
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- include/linux/sched_clock.h
+++ include/linux/sched_clock.h
@@ -10,8 +10,11 @@
 
 #ifdef CONFIG_GENERIC_SCHED_CLOCK
 extern void sched_clock_postinit(void);
+extern int sched_clock_suspend(void);
+extern void sched_clock_resume(void);
 #else
-static inline void sched_clock_postinit(void) { }
+static inline int sched_clock_suspend(void) { return 0; }
+static inline void sched_clock_resume(void) { }
 #endif
 
 extern void sched_clock_register(u64 (*read)(void), int bits,
