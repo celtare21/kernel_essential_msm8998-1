df0701e9cb1d5fd6f0a23e16fd062dcb7f86b923
Sultan Alsawaf <sultanxda@gmail.com>
2017-12-30 00:03:26 -0800

    cpufreq: Underclock performance cluster to 1958400 kHz
    Underclock the performance cluster from 2457600 kHz to 1958400 kHz for
    significant power savings. 1958400 kHz was determined empirically to be
    the optimal frequency to underclock to without significantly impacting
    performance, given the power consumption of the frequencies directly above
    and below it.
    
    The underclock can be disabled by specifying "cpufreq.disable_underclock=1"
    in the kernel command line. Note that we need to underclock like this
    instead of outright removing the higher frequencies because removing the
    higher frequencies outright causes a significant decrease in performance
    for some reason.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2246,6 +2246,9 @@ int cpufreq_get_policy(struct cpufreq_policy *policy, unsigned int cpu)
 }
 EXPORT_SYMBOL(cpufreq_get_policy);
 
+#define UNDERCLK_MAX_PERFCL 1958400
+static bool disable_underclock;
+
 /*
  * policy : current policy.
  * new_policy: policy to be set.
@@ -2255,6 +2258,13 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 {
 	struct cpufreq_governor *old_gov;
 	int ret;
+
+	if (!disable_underclock) {
+		if (new_policy->cpu > 3) {
+			if (new_policy->max > UNDERCLK_MAX_PERFCL)
+				new_policy->max = UNDERCLK_MAX_PERFCL;
+		}
+	}
 
 	pr_debug("setting new policy for CPU %u: %u - %u kHz\n",
 		 new_policy->cpu, new_policy->min, new_policy->max);
