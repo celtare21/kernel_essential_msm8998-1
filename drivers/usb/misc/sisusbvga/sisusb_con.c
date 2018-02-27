7fcd821c880f08f85529d05b39b4c62a0f945d8f
Kees Cook <keescook@chromium.org>
2018-02-26 16:04:18 -0800

    UPSTREAM: console: SisUSB2VGA: Drop dummy con_font_get()
    As done in commit:
    
      724ba8b30b04 ("console/dummy: leave .con_font_get set to NULL")
    
    This drops the dummy .con_font_get(), as it could leave arguments
    uninitialized.
    
    Cc: Thomas Winischhofer <thomas@winischhofer.net>
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Change-Id: I71d98ce1245f204c324c730a524c6e9a3ec26ef9
    (cherry picked from commit ea92110bc048848967bd2a046675a006005c4f06)
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- drivers/usb/misc/sisusbvga/sisusb_con.c
+++ drivers/usb/misc/sisusbvga/sisusb_con.c
@@ -1437,7 +1437,6 @@ static const struct consw sisusb_dummy_con = {
 	.con_switch =		SISUSBCONDUMMY,
 	.con_blank =		SISUSBCONDUMMY,
 	.con_font_set =		SISUSBCONDUMMY,
-	.con_font_get =		SISUSBCONDUMMY,
 	.con_font_default =	SISUSBCONDUMMY,
 	.con_font_copy =	SISUSBCONDUMMY,
 	.con_set_palette =	SISUSBCONDUMMY,
