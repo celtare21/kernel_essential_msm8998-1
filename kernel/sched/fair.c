7b6b7f5934535ed78e106a6f3ceef1063bf50e9e
Pavankumar Kondeti <pkondeti@codeaurora.org>
2018-03-02 08:58:29 +0530

    sched/fair: Don't consider isolated CPUs in find_best_target()
    Check if a CPU is isolated or not before considering it as a
    candidate CPU in find_best_target(). Another non isolated CPU
    is selected for user space tasks later but kernel threads
    wake up isolated CPUs unnecessarily.
    
    Change-Id: I6f888e66716d68bbadc68938121705ada32a904c
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6826,7 +6826,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			unsigned long wake_util, new_util, min_capped_util;
 			int idle_idx = INT_MAX;
 
-			if (!cpu_online(i))
+			if (!cpu_online(i) || cpu_isolated(i))
 				continue;
 
 			if (walt_cpu_high_irqload(i))
