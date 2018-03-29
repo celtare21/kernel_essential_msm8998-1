ecf8390781aac2b815f788a9572304e734c5ce6a
Pavankumar Kondeti <pkondeti@codeaurora.org>
2018-03-29 14:25:57 +0530

    sched/fair: Make tick path active migration more robust
    When a misfit task is marked for an active migration to a higher
    capacity CPU from the tick path, don't allow this task to be
    migrated to other CPUs from load balancer. This fixes the problem
    described below.
    
    (1) Task-A is running on CPU#0. This task does not fit on CPU#0
    (2) Task-A is marked for an active migration to CPU#4 from the
    tick path.
    (3) migration/0 is woken up on CPU#0 to push the Task-A to CPU#4.
    (4) CPU#0 has now two runnable tasks. migration/0 is just scheduled
    in.
    (5) Before migration/0 actually pushes the task to CPU#4, another
    CPU lets say CPU#1 whose capacity is same as CPU#0 is pulling the
    Task-A.
    (6) The Task-A misses an opportunity for upmigration.
    
    Change-Id: I7cc290f3f475caba73df9e7067b44e690c6f2ed7
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8084,6 +8084,10 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 		return 0;
 	}
 
+	/* Don't detach task if it is under active migration */
+	if (env->src_rq->push_task == p)
+		return 0;
+
 	/*
 	 * Aggressive migration if:
 	 * 1) destination numa is preferred
