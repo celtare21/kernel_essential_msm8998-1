14d27e3878e894bcf9f2ef80f28f31ce13446ba3
Mark Rutland <mark.rutland@arm.com>
2018-04-05 16:25:12 -0700

    kernel/fork.c: detect early free of a live mm
    KASAN splats indicate that in some cases we free a live mm, then
    continue to access it, with potentially disastrous results.  This is
    likely due to a mismatched mmdrop() somewhere in the kernel, but so far
    the culprit remains elusive.
    
    Let's have __mmdrop() verify that the mm isn't live for the current
    task, similar to the existing check for init_mm.  This way, we can catch
    this class of issue earlier, and without requiring KASAN.
    
    Currently, idle_task_exit() leaves active_mm stale after it switches to
    init_mm.  This isn't harmful, but will trigger the new assertions, so we
    must adjust idle_task_exit() to update active_mm.
    
    Link: http://lkml.kernel.org/r/20180312140103.19235-1-mark.rutland@arm.com
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -737,6 +737,8 @@ struct mm_struct *mm_alloc(void)
 void __mmdrop(struct mm_struct *mm)
 {
 	BUG_ON(mm == &init_mm);
+	WARN_ON_ONCE(mm == current->mm);
+	WARN_ON_ONCE(mm == current->active_mm);
 	mm_free_pgd(mm);
 	destroy_context(mm);
 	mmu_notifier_mm_destroy(mm);
