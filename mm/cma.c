09d5dbc34cc09aae3e22c536d31f5f23c895a591
Shiraz Hashim <shashim@codeaurora.org>
2018-05-01 17:58:51 +0530

    mm: cma: Increase retries if less blocks available
    If a particular cma region has lesser free blocks then
    increase retries to avoid allocation failure due to page
    being temporarily busy.
    
    Change-Id: I92021fd75315b266a978f7a5b0235344c800cba2
    Signed-off-by: Shiraz Hashim <shashim@codeaurora.org>
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- mm/cma.c
+++ mm/cma.c
@@ -411,6 +411,8 @@ struct page *cma_alloc(struct cma *cma, size_t count, unsigned int align,
 	struct page *page = NULL;
         int ret = -ENOMEM;
 	int retry_after_sleep = 0;
+	int max_retries = 2;
+	int available_regions = 0;
 
 	if (!cma || !cma->count)
 		return NULL;
@@ -437,16 +439,23 @@ struct page *cma_alloc(struct cma *cma, size_t count, unsigned int align,
 				bitmap_maxno, start, bitmap_count, mask,
 				offset);
 		if (bitmap_no >= bitmap_maxno) {
-			if (retry_after_sleep < 2) {
+			if (retry_after_sleep < max_retries) {
 				start = 0;
 				/*
-				* Page may be momentarily pinned by some other
-				* process which has been scheduled out, eg.
-				* in exit path, during unmap call, or process
-				* fork and so cannot be freed there. Sleep
-				* for 100ms and retry twice to see if it has
-				* been freed later.
-				*/
+				 * update max retries if available free regions
+				 * are less.
+				 */
+				if (available_regions < 3)
+					max_retries = 5;
+				available_regions = 0;
+				/*
+				 * Page may be momentarily pinned by some other
+				 * process which has been scheduled out, eg.
+				 * in exit path, during unmap call, or process
+				 * fork and so cannot be freed there. Sleep
+				 * for 100ms and retry twice to see if it has
+				 * been freed later.
+				 */
 				mutex_unlock(&cma->lock);
 				msleep(100);
 				retry_after_sleep++;
@@ -456,6 +465,8 @@ struct page *cma_alloc(struct cma *cma, size_t count, unsigned int align,
 				break;
 			}
 		}
+
+		available_regions++;
 		bitmap_set(cma->bitmap, bitmap_no, bitmap_count);
 		/*
 		 * It's safe to drop the lock here. We've marked this region for
