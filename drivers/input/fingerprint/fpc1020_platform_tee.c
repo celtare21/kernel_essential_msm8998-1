db4730c54526d5cc9cca6c20a50d703241ec02e4
Sultanxda <sultanxda@gmail.com>
2018-05-25 20:49:51 +0300

    fpc1020_tee: Use device struct as wakeup source
    Pt1: rework to use wakeups, instead of wakelocks.
    
    Pt2: There's no need to create a new wakeup source
    when every instance of struct device already comes with one.
    
    Use the device struct's built-in wakeup source to make things cleaner.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: YaroST12 <yaro330@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/input/fingerprint/fpc1020_platform_tee.c
+++ drivers/input/fingerprint/fpc1020_platform_tee.c
@@ -61,7 +61,6 @@ struct fpc1020_data {
 	struct pinctrl *fingerprint_pinctrl;
 	struct pinctrl_state *pinctrl_state[ARRAY_SIZE(pctl_names)];
 
-	struct wake_lock ttw_wl;
 	int irq_gpio;
 	int rst_gpio;
 	struct mutex lock; /* To set/get exported values in sysfs */
@@ -258,8 +257,7 @@ static irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 	dev_dbg(fpc1020->dev, "%s\n", __func__);
 
 	if (atomic_read(&fpc1020->wakeup_enabled)) {
-		wake_lock_timeout(&fpc1020->ttw_wl,
-					msecs_to_jiffies(FPC_TTW_HOLD_TIME));
+		pm_wakeup_event(fpc1020->dev, 5000);
 	}
 
 	sysfs_notify(&fpc1020->dev->kobj, NULL, dev_attr_irq.attr.name);
@@ -360,11 +358,9 @@ static int fpc1020_probe(struct platform_device *pdev)
 
 	atomic_set(&fpc1020->wakeup_enabled, 0);
 
-	irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT;
-	if (of_property_read_bool(dev->of_node, "fpc,enable-wakeup")) {
-		irqf |= IRQF_NO_SUSPEND;
-		device_init_wakeup(dev, 1);
-	}
+	irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT  | IRQF_PERF_CRITICAL;
+
+	device_init_wakeup(dev, true);
 
 	mutex_init(&fpc1020->lock);
 	rc = devm_request_threaded_irq(dev, gpio_to_irq(fpc1020->irq_gpio),
@@ -380,8 +376,6 @@ static int fpc1020_probe(struct platform_device *pdev)
 
 	/* Request that the interrupt should be wakeable */
 	enable_irq_wake(gpio_to_irq(fpc1020->irq_gpio));
-
-	wake_lock_init(&fpc1020->ttw_wl, WAKE_LOCK_SUSPEND, "fpc_ttw_wl");
 
 	rc = sysfs_create_group(&dev->kobj, &attribute_group);
 	if (rc) {
@@ -403,7 +397,6 @@ static int fpc1020_remove(struct platform_device *pdev)
 
 	sysfs_remove_group(&pdev->dev.kobj, &attribute_group);
 	mutex_destroy(&fpc1020->lock);
-	wake_lock_destroy(&fpc1020->ttw_wl);
 	dev_info(&pdev->dev, "%s\n", __func__);
 
 	return 0;
