862b423af6a1eab8fb3ade0b74fd9a54154a6959
Wei Wang <wvw@google.com>
2018-06-11 22:58:13 -0700

    scsi: ufs: call single_release to avoid memory leak
    Bug: 110044919
    Test: Build
    Change-Id: I9fa83b84cf52a54c24405f2416f8747a20901d3f
    Signed-off-by: Wei Wang <wvw@google.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/scsi/ufs/ufs-debugfs.c
+++ drivers/scsi/ufs/ufs-debugfs.c
@@ -1464,6 +1464,7 @@ static const struct file_operations ufsdbg_io_stats_desc = {
 	.open		= ufsdbg_io_stats_open,
 	.read		= seq_read,
 	.write		= ufsdbg_io_stats_write,
+	.release        = single_release,
 };
 
 static int ufsdbg_reset_controller_show(struct seq_file *file, void *data)
