4417915514ba7b725b265616efb5b4e944edb605
Yaroslav Furman <yaro330@gmail.com>
2018-07-21 23:58:36 +0300

    fpc1020_tee: remove FPC_TTW_HOLD_TIME definition
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/input/fingerprint/fpc1020_platform_tee.c
+++ drivers/input/fingerprint/fpc1020_platform_tee.c
@@ -36,8 +36,6 @@
 #include <linux/platform_device.h>
 #include <linux/wakelock.h>
 
-#define FPC_TTW_HOLD_TIME 1000
-
 #define RESET_LOW_SLEEP_MIN_US 5000
 #define RESET_LOW_SLEEP_MAX_US (RESET_LOW_SLEEP_MIN_US + 100)
 #define RESET_HIGH_SLEEP1_MIN_US 100
