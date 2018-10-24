06e483835171a9a0e98accb2fc1ffcae482eaf13
Sultan Alsawaf <sultanxda@gmail.com>
2018-10-23 19:18:25 -0700

    kernel: Boost CPU to the max when Pixel launcher becomes a top app
    Android moves the Pixel launcher process to the top-app cpuset when the
    multitasking view is entered since this launcher handles the multitasking
    interface, which is a very expensive interface on Pixel devices. Because of
    this, when rapidly hitting the multitask button and switching apps
    sometimes, there's noticeably bad stuttering due to the overhead from the
    app-switch interface itself.
    
    In order to fix the stuttering when multitasking, boost all CPUs and the
    DRAM bus to the max when the Pixel launcher is moved to the top-app cpuset.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -59,6 +59,8 @@
 #include <linux/delay.h>
 #include <linux/cpuset.h>
 #include <linux/atomic.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
 
 /*
  * pidlists linger the following amount before being destroyed.  The goal
@@ -2773,6 +2775,13 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
 	ret = cgroup_procs_write_permission(tsk, cgrp, of);
 	if (!ret)
 		ret = cgroup_attach_task(cgrp, tsk, threadgroup);
+
+	/* Boost CPU to the max for 500 ms when launcher becomes a top app */
+	if (!memcmp(tsk->comm, "s.nexuslauncher", sizeof("s.nexuslauncher")) &&
+		!memcmp(cgrp->kn->name, "top-app", sizeof("top-app")) && !ret) {
+		cpu_input_boost_kick_max(500);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
+	}
 
 	put_task_struct(tsk);
 	goto out_unlock_threadgroup;
