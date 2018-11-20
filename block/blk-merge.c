4600a2ba272c693fbaedc245983e895ccd42f4e4
Damien Le Moal <damien.lemoal@wdc.com>
2018-11-20 10:52:37 +0900

    block: prevent merging of requests with different priorities
    Growing in size a high priority request by merging it with a lower
    priority BIO or request will increase the request execution time. This
    is the opposite result of the desired effect of high I/O priorities,
    namely getting low I/O latencies. Prevent merging of requests and BIOs
    that have different I/O priorities to fix this.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@wdc.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- block/blk-merge.c
+++ block/blk-merge.c
@@ -788,6 +788,9 @@ static int attempt_merge(struct request_queue *q, struct request *req,
 	if (crypto_not_mergeable(req->bio, next->bio))
 		return 0;
 
+	if (req->ioprio != next->ioprio)
+		return 0;
+
 	/*
 	 * If we are allowed to merge, then append bio list
 	 * from next to rq and release next. merge_requests_fn
@@ -831,7 +834,6 @@ static int attempt_merge(struct request_queue *q, struct request *req,
 	 */
 	blk_account_io_merge(next);
 
-	req->ioprio = ioprio_best(req->ioprio, next->ioprio);
 	if (blk_rq_cpu_valid(next))
 		req->cpu = next->cpu;
 
@@ -899,6 +901,9 @@ bool blk_rq_merge_ok(struct request *rq, struct bio *bio)
 		return false;
 
 	if (crypto_not_mergeable(rq->bio, bio))
+		return false;
+
+	if (rq->ioprio != bio_prio(bio))
 		return false;
 
 	return true;
