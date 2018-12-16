08b82a444eee6ce8b341652f848167617f336f66
celtare21 <celtare21@gmail.com>
2018-12-16 11:37:21 +0000

    scsi: Remove derp
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -7796,7 +7796,6 @@ static void ufshcd_def_desc_sizes(struct ufs_hba *hba)
 }
 
 /**
->>>>>>> 965ca7bd57f6... scsi: ufs: read device descriptor once
  * ufshcd_probe_hba - probe hba to detect device and initialize
  * @hba: per-adapter instance
  *
