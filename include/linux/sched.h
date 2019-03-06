fa0cc843c5e9cc0bfcc10a2a38950c774cf452b9
Quentin Perret <quentin.perret@arm.com>
2019-03-06 09:40:27 +0000

    BACKPORT: sched/topology: Relocate arch_scale_cpu_capacity()
    By default, arch_scale_cpu_capacity() is only visible from within the
    kernel/sched folder. Relocate it to include/linux/sched.h to
    make it visible to other clients needing to know about the capacity of
    CPUs, such as the Energy Model framework.
    
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: adharmap@codeaurora.org
    Cc: chris.redpath@arm.com
    Cc: currojerez@riseup.net
    Cc: dietmar.eggemann@arm.com
    Cc: edubezval@gmail.com
    Cc: gregkh@linuxfoundation.org
    Cc: javi.merino@kernel.org
    Cc: joel@joelfernandes.org
    Cc: juri.lelli@redhat.com
    Cc: morten.rasmussen@arm.com
    Cc: patrick.bellasi@arm.com
    Cc: pkondeti@codeaurora.org
    Cc: rjw@rjwysocki.net
    Cc: skannan@codeaurora.org
    Cc: smuckle@google.com
    Cc: srinivas.pandruvada@linux.intel.com
    Cc: thara.gopinath@linaro.org
    Cc: tkjos@google.com
    Cc: valentin.schneider@arm.com
    Cc: vincent.guittot@linaro.org
    Cc: viresh.kumar@linaro.org
    Link: https://lkml.kernel.org/r/20181203095628.11858-2-quentin.perret@arm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    BACKPORT: Move arch_scale_cpu_capacity() to the sched.h header
    since topology.h doesn't exist in 4.9.
    
    Change-Id: I2b4c3d2faa007d87a0f788cd318fc730aafa6441
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    Signed-off-by: Artem Labazov <123321artyom@gmail.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -3896,4 +3896,15 @@ static inline void rseq_syscall(struct pt_regs *regs)
 
 #endif
 
+#ifndef arch_scale_cpu_capacity
+static __always_inline
+unsigned long arch_scale_cpu_capacity(struct sched_domain *sd, int cpu)
+{
+	if (sd && (sd->flags & SD_SHARE_CPUCAPACITY) && (sd->span_weight > 1))
+		return sd->smt_gain / sd->span_weight;
+
+	return SCHED_CAPACITY_SCALE;
+}
+#endif
+
 #endif
