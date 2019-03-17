9196f659563101493059c42c8d35f8107353ee19
celtare21 <celtare21@gmail.com>
2019-03-17 18:46:26 +0000

    Revert "rmnet_ipa: Fix netdev watchdog triggering on suspend"
    Sigh...
    
    This reverts commit 8e2014aa5fd3f465b75cf827a9a123321229b112.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/platform/msm/ipa/ipa_v3/rmnet_ipa.c
+++ drivers/platform/msm/ipa/ipa_v3/rmnet_ipa.c
@@ -2444,7 +2444,6 @@ static int rmnet_ipa_ap_suspend(struct device *dev)
 
 	/* Make sure that there is no Tx operation ongoing */
 	netif_stop_queue(netdev);
-	netif_device_detach(netdev);
 	ipa_rm_release_resource(IPA_RM_RESOURCE_WWAN_0_PROD);
 	ret = 0;
 
@@ -2470,10 +2469,8 @@ static int rmnet_ipa_ap_resume(struct device *dev)
 	struct net_device *netdev = IPA_NETDEV();
 
 	IPAWANDBG("Enter...\n");
-	if (netdev){
+	if (netdev)
 		netif_wake_queue(netdev);
-		netif_device_attach(netdev);
-	}
 	IPAWANDBG("Exit\n");
 
 	return 0;
