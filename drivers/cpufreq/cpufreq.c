8ce3f3c9633816b1dad98dab709c3822540c9493
celtare21 <celtare21@gmail.com>
2019-04-05 16:17:57 +0000

    cpufreq: Enforce minimum cpu freq policy
    Change Gold cluster minimum freq to 806400 and
    Silver culster minimum freq to 518400 in order
    to reduce jitter.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2392,6 +2392,11 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 	if (ret)
 		return ret;
 
+	if (cpumask_test_cpu(new_policy->cpu, cpu_lp_mask))
+		cpufreq_verify_within_limits(new_policy, 518400, new_policy->max);
+	else
+		cpufreq_verify_within_limits(new_policy, 806400, new_policy->max);
+
 	/* notification of the new policy */
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
 			CPUFREQ_NOTIFY, new_policy);
