16810ecb939f57acd8fa3be8389e8009c499b11e
celtare21 <celtare21@gmail.com>
2019-04-11 14:30:52 +0000

    cpufreq: schedutil: Enable iowait boost
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -823,7 +823,7 @@ static int sugov_init(struct cpufreq_policy *policy)
         tunables->up_rate_limit_us = 10000;
         tunables->down_rate_limit_us = 20000;
 
-	tunables->iowait_boost_enable = false;
+	tunables->iowait_boost_enable = true;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
