7d91af30a262f2dfc558da3bebaba0e47f0345e9
celtare21 <celtare21@gmail.com>
2019-05-13 15:20:20 +0000

    Treewide: Revert "Fix direct references to HZ" patchset
    Those are probably not needed and the drivers
    require direct HZ references.
    
    *Revert "drivers: platform: gsi: Fix direct references to HZ"
    *Revert "drivers: video: mdss: Fix direct references to HZ"
    *Revert "drivers: i2c: Fix direct references to HZ"
    *Revert "drivers: hwmon: Fix direct references to HZ"
    *Revert "drivers: ipa: Fix direct references to HZ"
    *Revert "fbdev: msm: Fix direct references to HZ"
    *Revert "sound: wcd_cpe: remove HZ dependency"
    *Revert "Convert few critical drivers to use msecs_to_jiffies insted of HZ"
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/misc/qcom/qdsp6v2/ultrasound/usf.c
+++ drivers/misc/qcom/qdsp6v2/ultrasound/usf.c
@@ -33,7 +33,7 @@
 #define USF_VERSION_ID 0x0171
 
 /* Standard timeout in the asynchronous ops */
-#define USF_TIMEOUT_JIFFIES 1000 /* 1 sec */
+#define USF_TIMEOUT_JIFFIES (1*HZ) /* 1 sec */
 
 /* Undefined USF device */
 #define USF_UNDEF_DEV_ID 0xffff
@@ -893,10 +893,9 @@ static int __usf_set_us_detection(struct usf_type *usf,
 						USF_ADSP_RESTART_STATE));
 	} else {
 		if (detect_info->detect_timeout == USF_DEFAULT_TIMEOUT)
-			timeout = msecs_to_jiffies(USF_TIMEOUT_JIFFIES);
+			timeout = USF_TIMEOUT_JIFFIES;
 		else
-			timeout = detect_info->detect_timeout *
-				msecs_to_jiffies(1000);
+			timeout = detect_info->detect_timeout * HZ;
 	}
 	rc = wait_event_interruptible_timeout(usf_xx->wait,
 					(usf_xx->us_detect_type !=
@@ -1138,7 +1137,7 @@ static int __usf_get_tx_update(struct usf_type *usf,
 		else {
 			prev_jiffies = jiffies;
 			if (upd_tx_info->timeout == USF_DEFAULT_TIMEOUT) {
-				timeout = msecs_to_jiffies(USF_TIMEOUT_JIFFIES);
+				timeout = USF_TIMEOUT_JIFFIES;
 				rc = wait_event_timeout(
 						usf_xx->wait,
 						(usf_xx->prev_region !=
@@ -1147,8 +1146,7 @@ static int __usf_get_tx_update(struct usf_type *usf,
 						 USF_WORK_STATE),
 						timeout);
 			} else {
-				timeout = upd_tx_info->timeout *
-					msecs_to_jiffies(1000);
+				timeout = upd_tx_info->timeout * HZ;
 				rc = wait_event_interruptible_timeout(
 						usf_xx->wait,
 						(usf_xx->prev_region !=
@@ -1249,7 +1247,7 @@ static int __usf_set_rx_update(struct usf_xx_type *usf_xx,
 			usf_xx->usc,
 			&(upd_rx_info->free_region)) ||
 		(usf_xx->usf_state == USF_IDLE_STATE),
-		 msecs_to_jiffies(USF_TIMEOUT_JIFFIES));
+		USF_TIMEOUT_JIFFIES);
 
 	if (!rc) {
 		rc = -ETIME;
