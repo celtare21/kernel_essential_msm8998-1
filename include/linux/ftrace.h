c63c68c631a780a0895c3f545c190e284bf74904
celtare21 <celtare21@gmail.com>
2019-05-26 20:47:05 +0000

    Revert "FROMLIST: tracing: Add support for preempt and irq enable/disable events"
    This reverts commit e5486e9c8991aa5b55959c96b6b375645b12dad6.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- include/linux/ftrace.h
+++ include/linux/ftrace.h
@@ -702,8 +702,7 @@ static inline void __ftrace_enabled_restore(int enabled)
   static inline void time_hardirqs_off(unsigned long a0, unsigned long a1) { }
 #endif
 
-#if defined(CONFIG_PREEMPT_TRACER) || \
-	(defined(CONFIG_DEBUG_PREEMPT) && defined(CONFIG_PREEMPTIRQ_EVENTS))
+#ifdef CONFIG_PREEMPT_TRACER
   extern void trace_preempt_on(unsigned long a0, unsigned long a1);
   extern void trace_preempt_off(unsigned long a0, unsigned long a1);
 #else
