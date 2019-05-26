afe6ea5854821243d014e3ec46f85623d970b701
celtare21 <celtare21@gmail.com>
2019-05-26 20:05:09 +0000

    Revert "time: Fix ktime_get_raw() incorrect base accumulation"
    This reverts commit 5e56f242721e98a18abc2436561a0e7ce44b4c85.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/time/timekeeping.c
+++ kernel/time/timekeeping.c
@@ -633,7 +633,9 @@ static inline void tk_update_ktime_data(struct timekeeper *tk)
 	tk->ktime_sec = seconds;
 
 	/* Update the monotonic raw base */
-	tk->tkr_raw.base = ns_to_ktime(tk->raw_sec * NSEC_PER_SEC);
+	seconds = tk->raw_sec;
+	nsec = (u32)(tk->tkr_raw.xtime_nsec >> tk->tkr_raw.shift);
+	tk->tkr_raw.base = ns_to_ktime(seconds * NSEC_PER_SEC + nsec);
 }
 
 /* must hold timekeeper_lock */
