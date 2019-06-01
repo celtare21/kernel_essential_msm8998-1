bd41b8f271f7f5297630bf79521fe84b4a2069ed
celtare21 <celtare21@gmail.com>
2019-06-01 23:00:16 +0300

    Revert "msm: kgsl: Add hibernation support"
    This reverts commit ed5f87f97d9df318cab215fb8b5a6cd264c377d7.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -738,11 +738,13 @@ static int kgsl_suspend_device(struct kgsl_device *device, pm_message_t state)
 	if (!device)
 		return -EINVAL;
 
+	KGSL_PWR_WARN(device, "suspend start\n");
 
 	mutex_lock(&device->mutex);
 	status = kgsl_pwrctrl_change_state(device, KGSL_STATE_SUSPEND);
 	mutex_unlock(&device->mutex);
 
+	KGSL_PWR_WARN(device, "suspend end\n");
 	return status;
 }
 
@@ -751,6 +753,7 @@ static int kgsl_resume_device(struct kgsl_device *device)
 	if (!device)
 		return -EINVAL;
 
+	KGSL_PWR_WARN(device, "resume start\n");
 	mutex_lock(&device->mutex);
 	if (device->state == KGSL_STATE_SUSPEND) {
 		kgsl_pwrctrl_change_state(device, KGSL_STATE_SLUMBER);
@@ -765,11 +768,11 @@ static int kgsl_resume_device(struct kgsl_device *device)
 			device->ftbl->idle(device);
 		kgsl_pwrctrl_change_state(device, KGSL_STATE_SLUMBER);
 		KGSL_PWR_ERR(device,
-			"resume invoked without a suspend, state = 0x%x\n",
-			device->state);
+			"resume invoked without a suspend\n");
 	}
 
 	mutex_unlock(&device->mutex);
+	KGSL_PWR_WARN(device, "resume end\n");
 	return 0;
 }
 
@@ -798,7 +801,8 @@ static int kgsl_runtime_resume(struct device *dev)
 }
 
 const struct dev_pm_ops kgsl_pm_ops = {
-	SET_SYSTEM_SLEEP_PM_OPS(kgsl_suspend, kgsl_resume)
+	.suspend = kgsl_suspend,
+	.resume = kgsl_resume,
 	.runtime_suspend = kgsl_runtime_suspend,
 	.runtime_resume = kgsl_runtime_resume,
 };
