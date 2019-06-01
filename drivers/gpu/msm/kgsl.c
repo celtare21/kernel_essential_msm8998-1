0d8016fe81120ca1942efcb89f2962f0b6ee3428
celtare21 <celtare21@gmail.com>
2019-06-01 22:54:32 +0300

    Revert "msm: kgsl: Bail out when two threads add sparse bindings"
    This reverts commit 5b4f3d16080fa294d95bc6d6af54d42b54f0f535.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -3481,16 +3481,10 @@ static int _sparse_add_to_bind_tree(struct kgsl_mem_entry *entry,
 		parent = *node;
 		this = rb_entry(parent, struct sparse_bind_object, node);
 
-		if ((new->v_off < this->v_off) &&
-			((new->v_off + new->size) <= this->v_off))
+		if (new->v_off < this->v_off)
 			node = &parent->rb_left;
-		else if ((new->v_off > this->v_off) &&
-			(new->v_off >= (this->v_off + this->size)))
+		else if (new->v_off > this->v_off)
 			node = &parent->rb_right;
-		else {
-			kfree(new);
-			return -EADDRINUSE;
-		}
 	}
 
 	rb_link_node(&new->node, parent, node);
