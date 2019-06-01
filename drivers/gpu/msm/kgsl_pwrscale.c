9ce30a8f4fa187f7f7fb450483b27aba6d56193a
celtare21 <celtare21@gmail.com>
2019-06-01 23:04:28 +0300

    Revert "msm: kgsl: Update total time at right place for accurate GPU busy"
    This reverts commit 9b0026e54f16dfc64d8bc8a566e333bfdaa314db.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/gpu/msm/kgsl_pwrscale.c
+++ drivers/gpu/msm/kgsl_pwrscale.c
@@ -367,7 +367,7 @@ int kgsl_devfreq_get_dev_status(struct device *dev,
 	struct kgsl_device *device = dev_get_drvdata(dev);
 	struct kgsl_pwrctrl *pwrctrl;
 	struct kgsl_pwrscale *pwrscale;
-	ktime_t tmp1, tmp2;
+	ktime_t tmp;
 
 	if (device == NULL)
 		return -ENODEV;
@@ -378,8 +378,6 @@ int kgsl_devfreq_get_dev_status(struct device *dev,
 	pwrctrl = &device->pwrctrl;
 
 	mutex_lock(&device->mutex);
-
-	tmp1 = ktime_get();
 	/*
 	 * If the GPU clock is on grab the latest power counter
 	 * values.  Otherwise the most recent ACTIVE values will
@@ -387,9 +385,9 @@ int kgsl_devfreq_get_dev_status(struct device *dev,
 	 */
 	kgsl_pwrscale_update_stats(device);
 
-	tmp2 = ktime_get();
-	stat->total_time = ktime_us_delta(tmp2, pwrscale->time);
-	pwrscale->time = tmp1;
+	tmp = ktime_get();
+	stat->total_time = ktime_us_delta(tmp, pwrscale->time);
+	pwrscale->time = tmp;
 
 	stat->busy_time = pwrscale->accum_stats.busy_time;
 
