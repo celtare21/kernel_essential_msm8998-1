57baeee1a0e88089e5e970ce28eebeb27c262c06
celtare21 <celtare21@gmail.com>
2019-06-01 22:40:22 +0300

    Revert "msm: kgsl: Add disable-wake-on-touch devicetree property"
    This reverts commit 8d2505b7dec0738d1b732a419b9b396b56cffc46.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -762,9 +762,6 @@ static int adreno_of_get_power(struct adreno_device *adreno_dev,
 	device->pwrctrl.bus_control = of_property_read_bool(node,
 		"qcom,bus-control");
 
-	device->pwrctrl.input_disable = of_property_read_bool(node,
-		"qcom,disable-wake-on-touch");
-
 	return 0;
 }
 
