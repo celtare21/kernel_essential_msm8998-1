e82b7a495e41e69433e83259f4731d24307384fa
Kuran Kaname <celtare21@gmail.com>
2019-06-07 00:03:54 +0300

    ion: Add missing {up/down}_read
    Signed-off-by: Kuran Kaname <celtare21@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -1733,6 +1733,7 @@ static int ion_debug_heap_show(struct seq_file *s, void *unused)
 	seq_printf(s, "%16s %16s %16s\n", "client", "pid", "size");
 	seq_puts(s, "----------------------------------------------------\n");
 
+	down_read(&dev->lock);
 	for (n = rb_first(&dev->clients); n; n = rb_next(n)) {
 		struct ion_client *client = rb_entry(n, struct ion_client,
 						     node);
@@ -1751,6 +1752,7 @@ static int ion_debug_heap_show(struct seq_file *s, void *unused)
 				   client->pid, size);
 		}
 	}
+	up_read(&dev->lock);
 	seq_puts(s, "----------------------------------------------------\n");
 	seq_puts(s, "orphaned allocations (info is from last known client):\n");
 	read_lock(&dev->buffer_lock);
