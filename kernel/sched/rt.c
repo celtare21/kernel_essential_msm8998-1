d80121970171c1184f37030203746c5c3c7ec969
celtare21 <celtare21@gmail.com>
2019-06-15 19:26:21 +0300

    sched: rt: Replace cpufreq_update_this_cpu with cpufreq_update_util
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1037,7 +1037,7 @@ static enum hrtimer_restart rt_schedtune_timer(struct hrtimer *timer)
 	rt_se->schedtune_enqueued = false;
 	schedtune_dequeue_task(p, cpu_of(rq));
 	sched_rt_update_capacity_req(rq);
-	cpufreq_update_this_cpu(rq, SCHED_CPUFREQ_RT);
+	cpufreq_update_util(rq, SCHED_CPUFREQ_RT);
 out:
 	raw_spin_unlock(&rq->lock);
 
@@ -1451,7 +1451,7 @@ enqueue_task_rt(struct rq *rq, struct task_struct *p, int flags)
 	rt_se->schedtune_enqueued = true;
 	schedtune_enqueue_task(p, cpu_of(rq));
 	sched_rt_update_capacity_req(rq);
-	cpufreq_update_this_cpu(rq, SCHED_CPUFREQ_RT);
+	cpufreq_update_util(rq, SCHED_CPUFREQ_RT);
 }
 
 static void dequeue_task_rt(struct rq *rq, struct task_struct *p, int flags)
@@ -1476,7 +1476,7 @@ static void dequeue_task_rt(struct rq *rq, struct task_struct *p, int flags)
 	rt_se->schedtune_enqueued = false;
 	schedtune_dequeue_task(p, cpu_of(rq));
 	sched_rt_update_capacity_req(rq);
-	cpufreq_update_this_cpu(rq, SCHED_CPUFREQ_RT);
+	cpufreq_update_util(rq, SCHED_CPUFREQ_RT);
 }
 
 /*
@@ -1571,7 +1571,7 @@ static void schedtune_dequeue_rt(struct rq *rq, struct task_struct *p)
 	rt_se->schedtune_enqueued = false;
 	schedtune_dequeue_task(p, task_cpu(p));
 	sched_rt_update_capacity_req(rq);
-	cpufreq_update_this_cpu(rq, SCHED_CPUFREQ_RT);
+	cpufreq_update_util(rq, SCHED_CPUFREQ_RT);
 }
 
 static int
