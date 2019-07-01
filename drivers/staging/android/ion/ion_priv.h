82df6ed33ea322d27270ec5edc91d1a32ab27f4b
Sultan Alsawaf <sultan@kerneltoast.com>
2019-06-30 20:40:59 -0700

    ion: Overhaul for vastly improved clarity and performance
    The ION driver suffers from massive code bloat caused by excessive
    debug features, as well as poor lock usage as a result of that. Multiple
    locks in ION exist to make the debug features thread-safe, which hurts
    ION's actual performance when doing its job.
    
    There are numerous code paths in ION that hold mutexes for no reason and
    hold them for longer than necessary. This results in not only unwanted
    lock contention, but also long delays when a mutex lock results in the
    calling thread getting preempted for a while. All lock usage in ION
    follows this pattern, which causes poor performance across the board.
    Furthermore, a single big lock is used mostly everywhere rather than
    multiple fine-grained locks.
    
    Most of the mutex locks can be replaced with simple atomic operations.
    Where a mutex lock can't be eliminated completely, a spinlock or rwlock
    can be used instead for quick operations, thereby avoiding long delays
    due to preemption. Fine-grained locks are also now used in place of the
    single big lock that was used before.
    
    Additionally, ion_dupe_sg_table is called very frequently, and lies
    within the rendering path for the display. Speed it up by reserving
    caches for its sg_table and page-sized scatterlist allocations, as well
    as by improving the sg copy process. Note that sg_alloc_table zeroes
    out `table`, so there's no need to zero it out using the memory
    allocator.
    
    Overall, just rewrite ION entirely to remedy its sorry situation. This
    optimizes ION for excellent performance and discards its rarely-used
    debug bloat.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/staging/android/ion/ion_priv.h
+++ drivers/staging/android/ion/ion_priv.h
@@ -86,18 +86,14 @@ struct ion_buffer {
 		void *priv_virt;
 		ion_phys_addr_t priv_phys;
 	};
-	struct mutex alloc_lock;
+	struct mutex kmap_lock;
 	struct mutex page_lock;
 	struct mutex vma_lock;
-	atomic_t kmap_cnt;
+	int kmap_cnt;
 	void *vaddr;
 	struct sg_table *sg_table;
 	struct page **pages;
 	struct list_head vmas;
-	/* used to track orphaned buffers */
-	atomic_t handle_count;
-	char task_comm[TASK_COMM_LEN];
-	pid_t pid;
 };
 void ion_buffer_destroy(struct ion_buffer *buffer);
 
@@ -201,29 +197,7 @@ struct ion_heap {
 	size_t free_list_size;
 	spinlock_t free_lock;
 	wait_queue_head_t waitqueue;
-	struct task_struct *task;
-
-	int (*debug_show)(struct ion_heap *heap, struct seq_file *, void *);
-	atomic_t total_allocated;
-	atomic_t total_handles;
 };
-
-/**
- * ion_buffer_cached - this ion buffer is cached
- * @buffer:		buffer
- *
- * indicates whether this ion buffer is cached
- */
-bool ion_buffer_cached(struct ion_buffer *buffer);
-
-/**
- * ion_buffer_fault_user_mappings - fault in user mappings of this buffer
- * @buffer:		buffer
- *
- * indicates whether userspace mappings of this buffer will be faulted
- * in, this can affect how buffers are allocated from the heap.
- */
-bool ion_buffer_fault_user_mappings(struct ion_buffer *buffer);
 
 /**
  * ion_device_create - allocates and returns an ion device
@@ -509,7 +483,7 @@ int ion_walk_heaps(struct ion_client *client, int heap_id,
 			enum ion_heap_type type, void *data,
 			int (*f)(struct ion_heap *heap, void *data));
 
-struct ion_handle *ion_handle_get_by_id(struct ion_client *client, int id);
+struct ion_handle *ion_handle_find_by_id(struct ion_client *client, int id);
 
 void ion_handle_put(struct ion_handle *handle);
 
@@ -517,5 +491,9 @@ bool ion_handle_validate(struct ion_client *client, struct ion_handle *handle);
 
 struct ion_buffer *get_buffer(struct ion_handle *handle);
 
+static inline bool ion_buffer_cached(struct ion_buffer *buffer)
+{
+	return buffer->flags & ION_FLAG_CACHED;
+}
 
 #endif /* _ION_PRIV_H */
