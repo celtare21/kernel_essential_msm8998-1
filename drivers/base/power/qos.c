e08851328051e2e1461b8e5f0c4b69e8dd2f7797
celtare21 <celtare21@gmail.com>
2019-08-06 14:41:18 +0000

    Revert "PM / QoS: Fix memory leak on resume_latency.notifiers"
    Breaks dongle and a man without a working dongle is a sad man.
    
    This reverts commit a64076d2e2248465c94b4c6b4ebce8f52638c8f1.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/base/power/qos.c
+++ drivers/base/power/qos.c
@@ -284,7 +284,7 @@ void dev_pm_qos_constraints_destroy(struct device *dev)
 	dev->power.qos = ERR_PTR(-ENODEV);
 	spin_unlock_irq(&dev->power.lock);
 
-	kfree(qos->resume_latency.notifiers);
+	kfree(c->notifiers);
 	kfree(qos);
 
  out:
