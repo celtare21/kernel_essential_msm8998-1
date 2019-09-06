2cbe25cf43d17e3b869cf342ffcecc91589a2dcf
Jean-Baptiste Théou <jb@essential.com>
2019-09-06 10:01:02 +0900

    Revert "ALSA: compress: Be more restrictive about when a drain is allowed"
    This patch introduce regression with QCOM offload path.
    Reverting for now
    
    This reverts commit 786c270d2a327d0efac2e5220e07591abe734eee.
    
    Change-Id: Ic43949cd67547575745990e4b7e1e8319a38d2de
    Signed-off-by: Jean-Baptiste Théou <jb@essential.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- sound/core/compress_offload.c
+++ sound/core/compress_offload.c
@@ -724,11 +724,7 @@ static int snd_compr_drain(struct snd_compr_stream *stream)
 	case SNDRV_PCM_STATE_OPEN:
 	case SNDRV_PCM_STATE_SETUP:
 	case SNDRV_PCM_STATE_PREPARED:
-	case SNDRV_PCM_STATE_PAUSED:
 		retval = -EPERM;
-		goto ret;
-	case SNDRV_PCM_STATE_XRUN:
-		retval = -EPIPE;
 		goto ret;
 	default:
 		break;
@@ -778,12 +774,8 @@ static int snd_compr_partial_drain(struct snd_compr_stream *stream)
 	case SNDRV_PCM_STATE_OPEN:
 	case SNDRV_PCM_STATE_SETUP:
 	case SNDRV_PCM_STATE_PREPARED:
-	case SNDRV_PCM_STATE_PAUSED:
 		mutex_unlock(&stream->device->lock);
 		return -EPERM;
-	case SNDRV_PCM_STATE_XRUN:
-		mutex_unlock(&stream->device->lock);
-		return -EPIPE;
 	default:
 		break;
 	}
