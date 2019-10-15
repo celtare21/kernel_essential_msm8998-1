0e50e152d7876300ce40119261a213be87464d54
celtare21 <celtare21@gmail.com>
2019-10-15 15:51:42 +0000

    power: qnpn-fg-gen3: Shut-up
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -3878,7 +3878,7 @@ static int fg_psy_get_property(struct power_supply *psy,
 		pval->intval = chip->ttf.cc_step.sel;
 		break;
 	default:
-		pr_err("unsupported property %d\n", psp);
+		pr_debug("unsupported property %d\n", psp);
 		rc = -EINVAL;
 		break;
 	}
